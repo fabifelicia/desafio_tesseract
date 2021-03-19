18d0eec896a74173f50d3b4ac10c0963b21c66fa
fabifelicia <fabiana.menezes@dcomp.ufs.br>
2021-03-19 08:34:19 -0300

    get api

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-let data = [];
+const URL = 'https://api.github.com/orgs/grupotesseract/public_members'
 
-async function getData() {
-  const results = await fetch("https://api.github.com/orgs/grupotesseract/public_members")
-    .then((response) => response.json())
-    .then((json) => json.results);
-  return results;
-}
-
-async function main() {
-  data = await getData();
-  console.log(data);  
-}
-
-main();
+fetch(URL)
+    .then((response) => response.json().then((data) => console.log(data)))
+    .catch((err) => console.error(err));
+  

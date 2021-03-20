1b277732ba5efa4741ae654f08f3b15a5ffecba2
fabifelicia <fabiana.menezes@dcomp.ufs.br>
2021-03-20 14:04:56 -0300

    filtering users by login

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
 const URL = 'https://api.github.com/orgs/grupotesseract/public_members'
 
 async function ListMembers() {
-     const members = await fetch(URL)
-        .then((response) => response.json())     
-        .catch((error) => {console.log(error)})
-    
-    return members
-    }
+  const members = await fetch(URL)
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log(error)
+    })
+
+  return members
+}
 
 const table = document.querySelector('#data-table tbody')
-table.innerHTML = "";
+table.innerHTML = ''
 
-ListMembers().then(members => {    
-    members.forEach((member) => {
-        const tr = document.createElement('tr')
-        tr.innerHTML = 
-        `
+ListMembers().then((members) => {
+  members.forEach((member) => {
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
             <td>
                 <img class="logo" src=${member.avatar_url}"  alt="User Image" />
             </td>
             <td class="login">${member.login}</td>
         `
-        table.appendChild(tr);
-    })
+    table.appendChild(tr)
+  })
 })
 
-function filterByLogin(member) {
-    
+function filterByLogin(login) {
+  ListMembers().then((members) => {
+    members.find((member) => {
+      if (member.login === login) {
+        table.innerHTML = ''
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+                    <td>
+                        <img class="logo" src=${member.avatar_url}"  alt="User Image" />
+                    </td>
+                    <td class="login">${member.login}</td>
+                `
+        table.appendChild(tr)
+      }
+    })
+  })
 }
 
+const btnFilter = document.querySelector('.btn')
 
-
-
- 
+btnFilter.addEventListener('click', () => {
+  const login = document.querySelector('input').value
+  filterByLogin(login)
+  document.querySelector('input').value = ''
+})
 

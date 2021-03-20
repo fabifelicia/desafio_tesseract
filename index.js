f2151aee9b355134f3410411f853c7daeafa2fc5
fabifelicia <fabiana.menezes@dcomp.ufs.br>
2021-03-20 10:06:37 -0300

    list members table

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const URL = 'https://api.github.com/orgs/grupotesseract/public_members'
 
-fetch(URL)
-    .then((response) => response.json().then((data) => console.log(data)))
-    .catch((err) => console.error(err));
-  
+async function ListMembers() {
+     const members = await fetch(URL)
+        .then((response) => response.json())     
+        .catch((error) => {console.log(error)})
+    
+    return members
+    }
+
+const table = document.querySelector('#data-table tbody')
+
+table.innerHTML = "";
+
+ListMembers().then(members => {
+    
+    members.forEach((member) => {
+        const tr = document.createElement('tr')
+        tr.innerHTML = 
+        `
+            <td>
+                <img class="logo" src=${member.avatar_url}"  alt="User Image" />
+            </td>
+            <td class="login">${member.login}</td>
+        `
+        table.appendChild(tr);
+    })
+
+})
+
+
+
+
+
+ 
+
